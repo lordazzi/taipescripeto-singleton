c30373d22a977b38be3b13606fd3b587595e02ad
RICARDO AZZI SILVA <ricardo.azzi-ext@viavarejo.com.br>
2020-04-23 17:44:38 -0300

    Rodando os testes em headless

--- karma.conf.js
+++ karma.conf.js
@@ -31,13 +31,23 @@ module.exports = function (config) {
     karmaTypescriptConfig: {
       tsconfig: "./tsconfig.json"
     },
+    customLaunchers: {
+      ChromeHeadless: {
+        base: 'Chrome',
+        flags: [
+          '--headless',
+          '--disable-gpu',
+          '--remote-debugging-port=9222',
+        ],
+      }
+    },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
+    browsers: ['ChromeHeadless'],
+    singleRun: true,
     restartOnFileChange: true
   });
 };
